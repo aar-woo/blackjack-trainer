86159a9159f41e3e46eb0d880cbc83fa9faf54c8
Aaron Woo <awoo@chapman.edu>
2022-11-09 15:40:15 -1000

    Created Table component and added basic styling for table.

---
+++ react-blackjack-trainer/src/components/Table.js
@@ -0,0 +1,14 @@
+import './Table.css';
+
+export default function Table() {
+
+    return (
+        <div className='table-container'>
+            <h2 className='title'>
+                Blackjack Trainer
+            </h2>
+            <div className='player-cards-zone' >
+            </div>
+        </div>
+    )
+}
