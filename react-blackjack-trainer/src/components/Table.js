604cda1e53ac5e2cb9a8081e5710194220b314ac
Aaron Woo <awoo@chapman.edu>
2022-12-04 14:50:15 -1000

    Added map function for dealer-hand div to return Card component for dealerHand cards as well.

--- react-blackjack-trainer/src/components/Table.js
+++ react-blackjack-trainer/src/components/Table.js
@@ -35,6 +35,9 @@ export default function Table() {
                 </h2>
                 <div className='dealer-cards-zone'>
                     <div className='dealer-hand'>
+                        {dealerHand.map((card) => {
+                            return (<Card value={card.value} suit={card.suit} ></Card>)
+                        })}
                     </div>
                     <div className='deck'>
                         <Deck deckLength={deck.getCards().length}/>
