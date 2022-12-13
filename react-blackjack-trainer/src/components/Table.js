e35a37486d5a5279d938013678806223fff829d7
Aaron Woo <awoo@chapman.edu>
2022-12-12 15:43:19 -1000

    Reset playerHasPair state when deal function is called.

--- react-blackjack-trainer/src/components/Table.js
+++ react-blackjack-trainer/src/components/Table.js
@@ -43,6 +43,7 @@ export default function Table() {
             setPlayerHand(playerHand);
             setDealerHand(dealerHand);
             setInProp(true);
+            setPlayerHasPair(false)
             setPlayerAction("");
         }
 
