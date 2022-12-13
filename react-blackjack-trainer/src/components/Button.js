c65b5edeb765624851a8659e82097ef52d56224d
Aaron Woo <awoo@chapman.edu>
2022-12-12 15:37:46 -1000

    Refactored Button component css and added hover and active states for buttons.

--- react-blackjack-trainer/src/components/Button.js
+++ react-blackjack-trainer/src/components/Button.js
@@ -4,6 +4,6 @@ export default function Button(props) {
     const {text, value, classNames, onClick : handleClick} = props;
 
     return (
-        <button className={classNames} value={value} onClick={handleClick}>{text}</button>
+        <button className={'btn ' + classNames} value={value} onClick={handleClick}>{text}</button>
     )
 }
