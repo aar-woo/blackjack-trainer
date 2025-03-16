5cae6aad09323dc9db5a1ff9d6d7c5e6d3ae0251
Aaron Woo <aaron.woo@intellibridge.us>
2025-03-16 13:18:43 -1000

    Add framer-motion package, create variants for initial positiong of card components based on index so player cards start on top of deck.

--- react-blackjack-trainer/src/components/Card.js
+++ react-blackjack-trainer/src/components/Card.js
@@ -1,42 +1,57 @@
-import './Card.css';
-import './utilities.css';
-import { useRef } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import "./Card.css";
+import "./utilities.css";
+import { useRef } from "react";
+import { CSSTransition } from "react-transition-group";
+import { motion } from "framer-motion";
 
 export default function Card(props) {
-    let {value, suit, face, additionalClasses, inProp} = props;
-    const nodeRef = useRef(null);
+  let { value, suit, face, additionalClasses, inProp, playerCardIndex } = props;
+  const nodeRef = useRef(null);
 
-    value = typeof value === 'string' ? value[0].toUpperCase() : value;
-    
-    if (face === 'back') {
-        return (
-        <div className="card-container card-back">
-            <img src='images/card-back.png'></img>
-        </div>
-        )
-    }
+  value = typeof value === "string" ? value[0].toUpperCase() : value;
 
+  const variants = {
+    0: {
+      x: 50,
+      y: -280,
+    },
+    1: {
+      x: 50,
+      y: -440,
+    },
+  };
+
+  if (face === "back") {
     return (
-        <CSSTransition nodeRef={nodeRef} in={inProp} appear={true} timeout={1200} classNames="card" >
-            <div ref={nodeRef} className={"card-container " + additionalClasses}>
-                <div className="row">
-                    <div className='card-value flex-column'>
-                        <p>{value}</p>
-                        <h3 className={`${suit}-icon`}></h3>
-                    </div>
-                </div>
-                <div className="row justify-center">
-                    <h1 className={`${suit}-icon`}></h1>
-                </div>
-                <div className="row justify-end">
-                    <div className='card-value flex-column'>
-                        <p>{value}</p>
-                        <h3 className={`${suit}-icon`}></h3>
-                    </div>
-                </div>
-            </div>
-        </CSSTransition>
+      <div className="card-container card-back">
+        <img src="images/card-back.png"></img>
+      </div>
+    );
+  }
 
-    )
+  return (
+    <motion.div
+      variants={variants}
+      initial={variants[playerCardIndex]}
+      animate={{}}
+    >
+      <div ref={nodeRef} className={"card-container " + additionalClasses}>
+        <div className="row">
+          <div className="card-value flex-column">
+            <p>{value}</p>
+            <h3 className={`${suit}-icon`}></h3>
+          </div>
+        </div>
+        <div className="row justify-center">
+          <h1 className={`${suit}-icon`}></h1>
+        </div>
+        <div className="row justify-end">
+          <div className="card-value flex-column">
+            <p>{value}</p>
+            <h3 className={`${suit}-icon`}></h3>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
--- react-blackjack-trainer/src/components/Table.js
+++ react-blackjack-trainer/src/components/Table.js
@@ -1,227 +1,263 @@
-import './Table.css';
-import Card from './Card';
+import "./Table.css";
+import Card from "./Card";
 import ReactCurvedText from "react-curved-text";
-import Button from './Button';
-import Deck from './Deck';
+import Button from "./Button";
+import Deck from "./Deck";
 import DeckClass from "../lib/deck.mjs";
-import { useEffect, useState, useRef } from 'react';
-import { compareHardTotal, compareSoftTotal, comparePair } from '../lib/basicStrategy';
-import { split } from 'lodash';
-import Modal from './Modal';
-
+import { useEffect, useState, useRef } from "react";
+import {
+  compareHardTotal,
+  compareSoftTotal,
+  comparePair,
+} from "../lib/basicStrategy";
+import { split } from "lodash";
+import Modal from "./Modal";
 
 export default function Table() {
-    const deckObj = new DeckClass();
-    deckObj.createShoe();
-    deckObj.shuffle();
-    const [deck, updateDeck] = useState(deckObj);
-    const [playerHand, setPlayerHand] = useState([]);
-    const [dealerHand, setDealerHand] = useState([]);
-    const [inProp, setInProp] = useState(false);
-    const [playerHasPair, setPlayerHasPair] = useState(false);
-    const [playerAction, setPlayerAction] = useState("");
-    const [result, setResult] = useState("");
-    const isMounted = useRef(false);
+  const deckObj = new DeckClass();
+  deckObj.createShoe();
+  deckObj.shuffle();
+  const [deck, updateDeck] = useState(deckObj);
+  const [playerHand, setPlayerHand] = useState([]);
+  const [dealerHand, setDealerHand] = useState([]);
+  const [inProp, setInProp] = useState(false);
+  const [playerHasPair, setPlayerHasPair] = useState(false);
+  const [playerAction, setPlayerAction] = useState("");
+  const [result, setResult] = useState("");
+  const isMounted = useRef(false);
 
-    const getPlayerAction = event => {
-        const buttonValue = event.target.value;
-        setPlayerAction(buttonValue);
+  const getPlayerAction = (event) => {
+    const buttonValue = event.target.value;
+    setPlayerAction(buttonValue);
+  };
+
+  function dealCards() {
+    const playerHand = [];
+    const dealerHand = [];
+
+    if (deck.cards.length < 52) {
+      deckObj.clear();
+      deckObj.createShoe();
+      deckObj.shuffle();
+      updateDeck(deckObj);
     }
 
-    function dealCards() {
-        const playerHand = [];
-        const dealerHand = [];
-
-        if (deck.cards.length < 52) {
-            deckObj.clear();
-            deckObj.createShoe();
-            deckObj.shuffle();
-            updateDeck(deckObj)
-        }
-
-        if (deck.cards.length < 312) {
-            removeCards();
-            setTimeout(deal, 100)
-        } else {
-            deal();
-        }
-
-        function deal() {
-            playerHand.push(deck.deal());
-            dealerHand.push(deck.deal());
-            playerHand.push(deck.deal());
-            setPlayerHand(playerHand);
-            setDealerHand(dealerHand);
-            setInProp(true);
-            setPlayerHasPair(false)
-            setPlayerAction("");
-            setResult("");
-        }
-
-        function removeCards() {
-            if (inProp) {
-                setInProp(false);
-                setPlayerHand([]);
-            }
-        }
+    if (deck.cards.length < 312) {
+      removeCards();
+      setTimeout(deal, 100);
+    } else {
+      deal();
     }
 
-    function checkForPair() {
-        if (playerHand.length === 0) {
-            return;
-        }
-        const faceCardsNotAce = ['king', 'queen', 'jack']
-        let firstCardVal = playerHand[0].value;
-        let secondCardVal = playerHand[1].value;
-
-        if (faceCardsNotAce.includes(firstCardVal)) firstCardVal = 10;
-        if (faceCardsNotAce.includes(secondCardVal)) secondCardVal = 10;
-
-        if (firstCardVal === secondCardVal) {
-            setPlayerHasPair(true);
-        }
+    function deal() {
+      playerHand.push(deck.deal());
+      dealerHand.push(deck.deal());
+      playerHand.push(deck.deal());
+      setPlayerHand(playerHand);
+      setDealerHand(dealerHand);
+      setInProp(true);
+      setPlayerHasPair(false);
+      setPlayerAction("");
+      setResult("");
     }
 
-    function getActionResult() {
-        if (isMounted.current === false) {
-            isMounted.current = true;
-            return;
-        }
-        if (playerHand.length <= 0) return;
+    function removeCards() {
+      if (inProp) {
+        setInProp(false);
+        setPlayerHand([]);
+      }
+    }
+  }
 
-        if (!playerAction) return;
+  function checkForPair() {
+    if (playerHand.length === 0) {
+      return;
+    }
+    const faceCardsNotAce = ["king", "queen", "jack"];
+    let firstCardVal = playerHand[0].value;
+    let secondCardVal = playerHand[1].value;
 
-        let dealerUpcard = dealerHand[0];
-        if (typeof dealerUpcard.value === 'string') {
-            dealerUpcard = dealerUpcard.value === 'ace' ? 11 : 10
-        } else {
-            dealerUpcard = dealerUpcard.value
-        }
+    if (faceCardsNotAce.includes(firstCardVal)) firstCardVal = 10;
+    if (faceCardsNotAce.includes(secondCardVal)) secondCardVal = 10;
 
-        let firstCardVal = playerHand[0].value;
-        let secondCardVal = playerHand[1].value;
+    if (firstCardVal === secondCardVal) {
+      setPlayerHasPair(true);
+    }
+  }
 
-        if (typeof firstCardVal === 'string') {
-            firstCardVal = firstCardVal === 'ace' ? 11 : 10;
-        }
-        if (typeof secondCardVal === 'string') {
-            secondCardVal = secondCardVal === 'ace' ? 11 : 10;
-        }
-        const playerTotal = firstCardVal + secondCardVal;
-        
-        /** 
-         * Check if the player's hand is a pair and call comparePair function and compare result with user action
-         * If the comparePair result is to not split, then we need to check if the player selected the correct action according to compareHardTotal 
-         **/
-        if (firstCardVal === secondCardVal) {
-            const splitResult = comparePair(dealerUpcard, firstCardVal);
-            if (splitResult) {
-                playerAction === 'split' ? setResult('correct') : setResult('incorrect');
-            } else if (splitResult == false && playerAction === 'split') {
-                setResult('incorrect')
-            } else {
-                const hardTotalResult = compareHardTotal(dealerUpcard, playerTotal);
-                playerAction[0] === hardTotalResult ? setResult('correct') : setResult('incorrect');
-            }
-        }
+  function getActionResult() {
+    if (isMounted.current === false) {
+      isMounted.current = true;
+      return;
+    }
+    if (playerHand.length <= 0) return;
 
-        /**
-         * Check if the player's hand is a soft total (has an ace, a card with value of 11)
-         **/
-        else if (firstCardVal === 11 || secondCardVal === 11) {
-            const softTotalResult = compareSoftTotal(dealerUpcard, playerTotal);
-            playerAction[0] === softTotalResult ? setResult('correct') : setResult('incorrect');
-        }
+    if (!playerAction) return;
 
-        else {
-            const hardTotalResult = compareHardTotal(dealerUpcard, playerTotal);
-            playerAction[0] === hardTotalResult ? setResult('correct') : setResult('incorrect');
-        }   
+    let dealerUpcard = dealerHand[0];
+    if (typeof dealerUpcard.value === "string") {
+      dealerUpcard = dealerUpcard.value === "ace" ? 11 : 10;
+    } else {
+      dealerUpcard = dealerUpcard.value;
     }
 
-    function displayResult() {
-        if (!result) return;
+    let firstCardVal = playerHand[0].value;
+    let secondCardVal = playerHand[1].value;
 
-        if (result === 'correct') {
-            return <Modal text='Correct' classNames='modal-green' />
-        } else {
-            return <Modal text='Incorrect' classNames='modal-red' />
-        }
+    if (typeof firstCardVal === "string") {
+      firstCardVal = firstCardVal === "ace" ? 11 : 10;
     }
+    if (typeof secondCardVal === "string") {
+      secondCardVal = secondCardVal === "ace" ? 11 : 10;
+    }
+    const playerTotal = firstCardVal + secondCardVal;
 
-    useEffect(checkForPair, [playerHand])
-    useEffect(getActionResult, [playerAction])
+    /**
+     * Check if the player's hand is a pair and call comparePair function and compare result with user action
+     * If the comparePair result is to not split, then we need to check if the player selected the correct action according to compareHardTotal
+     **/
+    if (firstCardVal === secondCardVal) {
+      const splitResult = comparePair(dealerUpcard, firstCardVal);
+      if (splitResult) {
+        playerAction === "split"
+          ? setResult("correct")
+          : setResult("incorrect");
+      } else if (splitResult == false && playerAction === "split") {
+        setResult("incorrect");
+      } else {
+        const hardTotalResult = compareHardTotal(dealerUpcard, playerTotal);
+        playerAction[0] === hardTotalResult
+          ? setResult("correct")
+          : setResult("incorrect");
+      }
+    } else if (firstCardVal === 11 || secondCardVal === 11) {
+      /**
+       * Check if the player's hand is a soft total (has an ace, a card with value of 11)
+       **/
+      const softTotalResult = compareSoftTotal(dealerUpcard, playerTotal);
+      playerAction[0] === softTotalResult
+        ? setResult("correct")
+        : setResult("incorrect");
+    } else {
+      const hardTotalResult = compareHardTotal(dealerUpcard, playerTotal);
+      playerAction[0] === hardTotalResult
+        ? setResult("correct")
+        : setResult("incorrect");
+    }
+  }
 
-    return (
-        <div className='table-container'>
-            <div className='game-container'>
-                <h2 className='title'>
-                    Blackjack Trainer
-                </h2>
-                <div className='dealer-cards-zone'>
-                    <div className='dealer-hand'>
-                        {dealerHand.map((card, index) => {
-                            return (<Card value={card.value} suit={card.suit} key={index} />)
-                        })}
-                    </div>
-                    <div className='deck'>
-                        <Deck deckLength={deck.getCards().length} />
-                    </div>
-                </div>
-                <div className='table-info'>
-                    <ReactCurvedText
-                        width={380}
-                        height={70}
-                        cx={185}
-                        cy={-30}
-                        rx={140}
-                        ry={100}
-                        startOffset={72}
-                        reversed={false}
-                        text="BLACK JACK PAYS 3 TO 2"
-                        textProps={{ style: { fontSize: 24 } }}
-                        textPathProps={{ "fill": "#a3a3a3" }}
-                        tspanProps={null}
-                        ellipseProps={null}
-                        svgProps={null}
-                    />
-                    <ReactCurvedText
-                        width={380}
-                        height={50}
-                        cx={187}
-                        cy={-55}
-                        rx={140}
-                        ry={100}
-                        startOffset={90}
-                        reversed={false}
-                        text="DEALER MUST HIT SOFT 17"
-                        textProps={{ style: { fontSize: 18 } }}
-                        textPathProps={{ "fill": "#ffffff" }}
-                        tspanProps={null}
-                        ellipseProps={null}
-                        svgProps={null}
-                    />
-                </div>
-                <div className='player-cards-zone'>
-                    {playerHand.map((card, index) => {
-                        let classes;
-                        index === 0 ? classes = 'underlap' : classes = 'overlap';
-                        return <Card key={index} value={card.value} suit={card.suit} additionalClasses={classes} inProp={inProp} />
-                    })}
-                    {displayResult()}
-                </div>
-            </div>
-            <div className='actions-container'>
-                <div className='deal-btn-container'>
-                    <Button text='Deal Cards' onClick={dealCards} />
-                </div>
-                <div className='game-actions'>
-                    <Button text='Hit' classNames='action-btn bg-green' onClick={getPlayerAction} value='hit' />
-                    <Button text='Stand' classNames='action-btn bg-red' onClick={getPlayerAction} value='stand' />
-                    <Button text='Double' classNames='action-btn bg-grey' onClick={getPlayerAction} value='double' />
-                    {playerHasPair === true && <Button text='Split' classNames='action-btn bg-grey' onClick={getPlayerAction} value='split' />}
-                </div>
-            </div>
+  function displayResult() {
+    if (!result) return;
+
+    if (result === "correct") {
+      return <Modal text="Correct" classNames="modal-green" />;
+    } else {
+      return <Modal text="Incorrect" classNames="modal-red" />;
+    }
+  }
+
+  useEffect(checkForPair, [playerHand]);
+  useEffect(getActionResult, [playerAction]);
+
+  return (
+    <div className="table-container">
+      <div className="game-container">
+        <h2 className="title">Blackjack Trainer</h2>
+        <div className="dealer-cards-zone">
+          <div className="dealer-hand">
+            {dealerHand.map((card, index) => {
+              return <Card value={card.value} suit={card.suit} key={index} />;
+            })}
+          </div>
+          <div className="deck">
+            <Deck deckLength={deck.getCards().length} />
+          </div>
         </div>
-    )
+        <div className="table-info">
+          <ReactCurvedText
+            width={380}
+            height={70}
+            cx={185}
+            cy={-30}
+            rx={140}
+            ry={100}
+            startOffset={72}
+            reversed={false}
+            text="BLACK JACK PAYS 3 TO 2"
+            textProps={{ style: { fontSize: 24 } }}
+            textPathProps={{ fill: "#a3a3a3" }}
+            tspanProps={null}
+            ellipseProps={null}
+            svgProps={null}
+          />
+          <ReactCurvedText
+            width={380}
+            height={50}
+            cx={187}
+            cy={-55}
+            rx={140}
+            ry={100}
+            startOffset={90}
+            reversed={false}
+            text="DEALER MUST HIT SOFT 17"
+            textProps={{ style: { fontSize: 18 } }}
+            textPathProps={{ fill: "#ffffff" }}
+            tspanProps={null}
+            ellipseProps={null}
+            svgProps={null}
+          />
+        </div>
+        <div className="player-cards-zone">
+          {playerHand.map((card, index) => {
+            let classes;
+            // index === 0 ? (classes = "underlap") : (classes = "overlap");
+            return (
+              <Card
+                key={index}
+                value={card.value}
+                suit={card.suit}
+                // additionalClasses={classes}
+                inProp={inProp}
+                playerCardIndex={index}
+              />
+            );
+          })}
+          {displayResult()}
+        </div>
+      </div>
+      <div className="actions-container">
+        <div className="deal-btn-container">
+          <Button text="Deal Cards" onClick={dealCards} />
+        </div>
+        <div className="game-actions">
+          <Button
+            text="Hit"
+            classNames="action-btn bg-green"
+            onClick={getPlayerAction}
+            value="hit"
+          />
+          <Button
+            text="Stand"
+            classNames="action-btn bg-red"
+            onClick={getPlayerAction}
+            value="stand"
+          />
+          <Button
+            text="Double"
+            classNames="action-btn bg-grey"
+            onClick={getPlayerAction}
+            value="double"
+          />
+          {playerHasPair === true && (
+            <Button
+              text="Split"
+              classNames="action-btn bg-grey"
+              onClick={getPlayerAction}
+              value="split"
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
