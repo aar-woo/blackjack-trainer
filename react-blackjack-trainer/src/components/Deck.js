396e63ba4878ddb16a5f459151c6dee49643734d
aar-woo <awooyes@gmail.com>
2025-03-19 10:21:24 -1000

    Fix gap left in deck for initial y caused by middle card shuffling to top.

--- react-blackjack-trainer/src/components/Deck.js
+++ react-blackjack-trainer/src/components/Deck.js
@@ -8,22 +8,33 @@ export default function Deck(props) {
   const deckCards = Array.from({ length: 10 }, (_, i) => i);
   const [shuffleComplete, setShuffleComplete] = useState(false);
 
+  const getInitialY = (index) => {
+    const spacingMult = -1.8;
+    if (index === 4) {
+      return spacingMult * 8;
+    }
+    if (index < 4) {
+      return spacingMult * index;
+    } else {
+      return spacingMult * (index - 1);
+    }
+  };
 
   return (
     <>
       <div>
-      {deckCards.map((index) => {
-        return (
-          <motion.div
-            key={index}
-            className="stacked-card"
-            initial={{
-              x: 0,
-              y: index * -2,
-            }}
-            animate={{
-              x: [0, index % 2 === 0 ? 80 : -80, 0],
-            }}
+        {deckCards.map((index) => {
+          return (
+            <motion.div
+              key={index}
+              className="stacked-card"
+              initial={{
+                x: 0,
+                y: getInitialY(index),
+              }}
+              animate={{
+                x: [0, index % 2 === 0 ? 80 : -80, 0],
+              }}
               transition={{
                 delay: index === 4 ? 1 : index * 0.1,
               }}
@@ -33,12 +44,12 @@ export default function Deck(props) {
               style={{
                 zIndex: shuffleComplete && index === 4 ? 10 : 0,
               }}
-          >
-            <Card face="back" />
-          </motion.div>
-        );
-      })}
-      <Card face="back" />
+            >
+              <Card face="back" />
+            </motion.div>
+          );
+        })}
+        <Card face="back" />
       </div>
     </>
   );
